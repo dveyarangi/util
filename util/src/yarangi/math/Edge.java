2e2c4aea487f53a8f4b74eba9be432470417a1ca
dveyarangi <dveyarangi@gmail.com>
2013-06-18 22:50:33 +0300

    Delaunay2d with bugs

---
+++ util/src/yarangi/math/Edge.java
@@ -0,0 +1,11 @@
+package yarangi.math;
+
+public class Edge extends Pair <IVector2D>
+{
+
+	public Edge(IVector2D first, IVector2D second)
+	{
+		super( first, second );
+	}
+	
+}
--- util/src/yarangi/math/Geometry.java
+++ util/src/yarangi/math/Geometry.java
@@ -3,6 +3,14 @@ package yarangi.math;
 public class Geometry 
 {
 	
+	public static void main(String [] args)
+	{
+//		System.out.println(pointInTriangleSameSide( new Vector2D(2,1), new Vector2D(0,0), new Vector2D(0,1), new Vector2D(2,1) ));
+//		System.out.println(calcDistanceToLine(Vector2D.ZERO(), Vector2D.R(-1, 1), Vector2D.R(5, 0)));
+		System.out.println(pointInCircumCirle( new Vector2D(-0.5,0.5), new Vector2D(-2,0), new Vector2D(0,2), new Vector2D(2,0) ));
+	}
+	
+	
 	/**
 	 * Calculates distance value square.
 	 * @param v1
@@ -45,11 +53,6 @@ public class Geometry
 		return P.minus(Q).abs() / v.left().abs();
 	}
 	
-	public static void main(String [] args)
-	{
-		
-		System.out.println(calcDistanceToLine(Vector2D.ZERO(), Vector2D.R(-1, 1), Vector2D.R(5, 0)));
-	}
 	public static double calcTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3)
 	{
 		return (x1*y2-x2*y1 + x2*y3-x3*y2 - x3*y1-x1*y3) / 2;
@@ -60,7 +63,19 @@ public class Geometry
 		return calcTriangleArea(x1, y1, x2, y2, px, py) > 0;
 	}
 	
-	public static boolean isInCircle(double px, double py, double x1, double y1, double x2, double y2, double x3, double y3)
+	/**
+	 * A, B, and C must be ordered clockwise.
+	 * @param p
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public static boolean pointInCircumCirle(IVector2D p, IVector2D a, IVector2D b, IVector2D c )
+	{
+		return pointInCircumCirle( p.x(), p.y(), a.x(), a.y(), b.x(), b.y(), c.x(), c.y() );
+	}
+	public static boolean pointInCircumCirle(double px, double py, double x1, double y1, double x2, double y2, double x3, double y3)
 	{
 		double d1 = x1*x1+y1*y1;
 		double d2 = x2*x2+y2*y2;
@@ -69,7 +84,7 @@ public class Geometry
 		return determinant(x2, y2, d2, x3, y3, d3, px, py, pd)
 		     - determinant(x1, y1, d1, x3, y3, d3, px, py, pd)
 		     + determinant(x1, y1, d1, x2, y2, d2, px, py, pd)
-		     - determinant(x1, y1, d1, x2, y2, d2, x3, y3, d3) < 0;
+		     - determinant(x1, y1, d1, x2, y2, d2, x3, y3, d3) <= 0;
 	}
 	
 	public static double determinant(double x1, double y1, double x2, double y2)
@@ -124,4 +139,35 @@ public class Geometry
 	
 		return Vector2D.R(s, t);
 	}
+	
+	public static boolean pointInTriangle(IVector2D p, IVector2D a, IVector2D b, IVector2D c)
+	{
+		return pointInTriangleSameSide(p, a, b, c);
+	}
+	
+	
+	/**
+	 * Checking that cross products between following vectors pairs have the same sign:
+	 * <li> vertex(n+1) - vertex(n)
+	 * <li> point - vertex(n)
+	 *  
+	 * (basically, angle between the pair of vectors have similar clockwise offset direction) 
+	 * 
+	 * @param p - tested point
+	 * @param a - triangle vertex A
+	 * @param b - triangle vertex B
+	 * @param c - triangle vertex C
+	 * @return
+	 */
+	private static boolean pointInTriangleSameSide(IVector2D p, IVector2D a, IVector2D b, IVector2D c)
+	{
+		double cross1 = ( (b.x() - a.x()) * (p.y() - a.y()) ) - ( (b.y() - a.y()) * (p.x() - a.x()) );  
+		double cross2 = ( (c.x() - b.x()) * (p.y() - b.y()) ) - ( (c.y() - b.y()) * (p.x() - b.x()) );  
+		double cross3 = ( (a.x() - c.x()) * (p.y() - c.y()) ) - ( (a.y() - c.y()) * (p.x() - c.x()) );
+		
+		return (cross1 <= 0 && cross2 <= 0 && cross3 <= 0) 
+			|| (cross1 >= 0 && cross2 >= 0 && cross3 >= 0);
+	}
+	
+	
 }
